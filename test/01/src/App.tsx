a93eb460554c399176f5cf18f69c4d88fd27fb16
Abhishek Raj <abhi@raj.me>
2022-01-14 05:25:04 +0530

    Add test folder (CRA)

---
+++ test/01/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'starter-ui';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="appHeader">
+        <h3>List of Components</h3>
+        <Button>My Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
